9f06d03a567560a3a1c9e64f6350b187fc46c316
LanceShu <lanceshu@lanceshu.test.com>
2017-11-28 13:35:37 +0800

    登陆界面完成

--- src/main/java/Main/View/MainView.java
+++ src/main/java/Main/View/MainView.java
@@ -7,10 +7,72 @@ public class MainView {
 
     public static void main(String[] args) {
 
-        //ATM机GUI界面;
-        GUI.ATMGUI();
+        mainView();
 
     }
 
+    private static void mainView(){
+        JFrame frame = new JFrame("ATM登陆界面");
+        //设置可以关闭;
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //设置是否可见;
+        frame.setVisible(true);
+        //设置宽度与高度;
+        frame.setSize(600,600);
+        //设置显示位置;
+        frame.setLocation(200,100);
+
+        //生成一个容器;
+        Container container = frame.getContentPane();
+        //容器的布局样式;
+        container.setLayout(new GridLayout(7,1));
+
+        //第一个版面;
+        JPanel panel0 = new JPanel();
+        container.add(panel0);
+
+        //第二个版面;
+        JPanel panel1 = new JPanel();
+        //版面上的标题;
+        JLabel title = new JLabel("欢迎登陆ATM系统");
+        //标题的自体样式;
+        title.setFont(new Font("宋体",Font.BOLD,20));
+        panel1.add(title);
+        container.add(panel1);
+
+        JPanel panel2 = new JPanel();
+        //文本框：银行卡号;
+        JLabel name = new JLabel("银行卡号：");
+        name.setFont(new Font("宋体",Font.BOLD,16));
+        panel2.add(name);
+        //文本输入框,长度为45;
+        TextField tf_name = new TextField(45);
+        tf_name.setText("请在此处输入银行卡号");
+        panel2.add(tf_name);
+        container.add(panel2);
+
+
+        JPanel panel3 = new JPanel();
+        //文本框：密码;
+        JLabel pass = new JLabel("卡号密码：");
+        pass.setFont(new Font("宋体",Font.BOLD,16));
+        panel3.add(pass);
+        //文本输入框,长度为45;
+        TextField tf_pass = new TextField(45);
+        tf_pass.setText("请在此处输入银行卡密码");
+        panel3.add(tf_pass);
+        container.add(panel3);
+
+        JPanel panel4 = new JPanel();
+        //确认按钮；
+        JButton bt_log = new JButton("确认");
+        panel4.add(bt_log);
+        //退出按钮;
+        JButton bt_exit = new JButton("退出");
+        panel4.add(bt_exit);
+        container.add(panel4);
+
+
+    }
 
 }
--- src/main/java/Main/View/MenuView.java
+++ src/main/java/Main/View/MenuView.java
@@ -1,75 +1,70 @@
 package Main.View;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class GUI {
+public class MenuView {
 
     public static void ATMGUI(){
 
         //主布局;
-        Frame bodyFrame = new Frame("ATM");
+        JFrame bodyFrame = new JFrame("ATM");
+        bodyFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //设置主布局的布局格式;
         bodyFrame.setLayout(new BorderLayout());
 
         //左边的布局;
-        Panel leftFrame = new Panel();
+        JPanel leftFrame = new JPanel();
         //左边布局功能的布局格式;
         leftFrame.setLayout(new BorderLayout());
         //转帐按钮;
-        Button bt1 = new Button("转帐");
+        JButton bt1 = new JButton("转帐");
+        bt1.setBorderPainted(false);
         //转帐按钮的大小;
-        bt1.setPreferredSize(new Dimension(150,80));
+        bt1.setPreferredSize(new Dimension(150,140));
         //转帐按钮的颜色;
-        bt1.setBackground(Color.gray);
         //查询余额按钮;
-        Button bt2 = new Button("查询余额");
+        JButton bt2 = new JButton("查询余额");
+        bt1.setBorderPainted(false);
         //查询余额按钮的大小;
-        bt2.setPreferredSize(new Dimension(150,80));
+        bt2.setPreferredSize(new Dimension(150,140));
         //查询余额按钮的颜色;
-        bt2.setBackground(Color.gray);
         //按钮在布局中的位置;
         leftFrame.add(bt1,BorderLayout.NORTH);
         leftFrame.add(bt2,BorderLayout.SOUTH);
 
         //右边的布局;
-        Panel rightFrame = new Panel();
+        JPanel rightFrame = new JPanel();
         //右边布局的布局格式;
         rightFrame.setLayout(new BorderLayout());
         //取款按钮；
-        Button bt3 = new Button("取款");
+        JButton bt3 = new JButton("取款");
+        bt1.setBorderPainted(false);
         //取款按钮的大小；
-        bt3.setPreferredSize(new Dimension(150,80));
+        bt3.setPreferredSize(new Dimension(150,140));
         //取款按钮的颜色；
-        bt3.setBackground(Color.gray);
         //存款按钮;
-        Button bt4 = new Button("存款");
+        JButton bt4 = new JButton("存款");
         //存款按钮的大小；
-        bt4.setPreferredSize(new Dimension(150,80));
+        bt4.setPreferredSize(new Dimension(150,140));
+        bt1.setBorderPainted(false);
         //存款按钮的颜色；
-        bt4.setBackground(Color.gray);
         //按钮在布局中的位置；
         rightFrame.add(bt3,BorderLayout.NORTH);
         rightFrame.add(bt4,BorderLayout.SOUTH);
 
         //中间显示内容部分;
-        TextField area = new TextField(20);
-        area.setPreferredSize(new Dimension(200,400));
+        JTextField area = new JTextField(20);
+        area.setPreferredSize(new Dimension(200,300));
 
         bodyFrame.add(leftFrame,BorderLayout.WEST);
         bodyFrame.add(rightFrame,BorderLayout.EAST);
         bodyFrame.add(area,BorderLayout.CENTER);
 
-        bodyFrame.setSize(600,400);
+        bodyFrame.setSize(600,300);
         bodyFrame.setVisible(true);
-
-        bodyFrame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
 
     }
 }
